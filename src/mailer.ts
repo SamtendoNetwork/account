6cb6dd445d7e83894af6e058b2f63329d944e3e8
Sam <rdp@samtendo.net>
2026-05-26 17:42:59 +0100

    Finally fix email system and update templates

--- src/mailer.ts
+++ src/mailer.ts
@@ -3,7 +3,7 @@
 import path from 'node:path';
 import fs from 'node:fs';
 import nodemailer from 'nodemailer';
-import { config, disabledFeatures } from '@/config-manager';
+import { config } from '@/config-manager';
 import { MailerOptions } from '@/types/common/mailer-options';
 
 const genericEmailTemplate = fs.readFileSync(
@@ -16,57 +16,51 @@ const confirmationEmailTemplate = fs.readFileSync(
 	'utf8'
 );
 
-let transporter: nodemailer.Transporter;
-
-if (true) {
-	transporter = nodemailer.createTransport({
-		host: config.email.host,
-		port: config.email.port,
-		secure: config.email.secure,
-		auth: {
-			user: config.email.auth.user,
-			pass: config.email.auth.pass
-		}
-	});
-}
+const transporter = nodemailer.createTransport({
+	host: 'smtp.zoho.eu',
+	port: 465,
+	secure: true,
+	auth: {
+		user: config.email.auth.user,
+		pass: config.email.auth.pass
+	}
+});
 
 export async function sendMail(options: MailerOptions): Promise<void> {
-	if (true) {
-		const {
-			to,
-			subject,
-			username,
-			paragraph,
-			preview,
-			text,
-			link,
-			confirmation
-		} = options;
+	const {
+		to,
+		subject,
+		username,
+		paragraph,
+		preview,
+		text,
+		link,
+		confirmation
+	} = options;
 
-		let html = confirmation
-			? confirmationEmailTemplate
-			: genericEmailTemplate;
+	let html = confirmation
+		? confirmationEmailTemplate
+		: genericEmailTemplate;
 
-		html = html.replace(/{{username}}/g, username);
-		html = html.replace(/{{paragraph}}/g, paragraph || '');
-		html = html.replace(/{{preview}}/g, preview || '');
-		html = html.replace(/{{confirmation-href}}/g, confirmation?.href || '');
-		html = html.replace(/{{confirmation-code}}/g, confirmation?.code || '');
+	html = html.replace(/{{username}}/g, username);
+	html = html.replace(/{{paragraph}}/g, paragraph || '');
+	html = html.replace(/{{preview}}/g, preview || '');
+	html = html.replace(/{{confirmation-href}}/g, confirmation?.href || '');
+	html = html.replace(/{{confirmation-code}}/g, confirmation?.code || '');
 
-		if (link) {
-			const { href, text } = link;
+	if (link) {
+		const { href, text } = link;
 
-			const button = `<tr><td width="100%" height="16px" style="line-height: 16px;">&nbsp;</td></tr><tr><td class="confirm-link" bgcolor="#673db6" style="font-size: 14px; font-weight: 700; border-radius: 10px; padding: 12px" align="center"><a href="${href}" style="text-decoration: none; color: #ffffff;" width="100%">${text}</a></td></tr>`;
+		const button = `<tr><td width="100%" height="16px" style="line-height: 16px;">&nbsp;</td></tr><tr><td class="confirm-link" bgcolor="#2ca8ce" style="font-size: 14px; font-weight: 700; border-radius: 10px; padding: 12px" align="center"><a href="${href}" style="text-decoration: none; color: #ffffff;" width="100%">${text}</a></td></tr>`;
 
-			html = html.replace(/<!--{{buttonPlaceholder}}-->/g, button);
-		}
-
-		await transporter.sendMail({
-			from: config.email.from,
-			to,
-			subject,
-			text,
-			html
-		});
+		html = html.replace(/<!--{{buttonPlaceholder}}-->/g, button);
 	}
+
+	await transporter.sendMail({
+		from: config.email.from,
+		to,
+		subject,
+		text,
+		html
+	});
 }
